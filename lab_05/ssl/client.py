798da14fc9693741faafff7b6c00540348c652d6
tranlongvu-02-10 <tranlongvu02102004@gmail.com>
2025-04-05 16:01:26 +0700

    Update

--- lab_05/ssl/client.py
+++ lab_05/ssl/client.py
@@ -2,6 +2,7 @@ import socket
 import ssl
 import threading
 
+
 server_address = ('localhost', 12345)
 
 def receive_data(ssl_socket):
@@ -10,30 +11,37 @@ def receive_data(ssl_socket):
             data = ssl_socket.recv(1024)
             if not data:
                 break
-            print("Nhận:", data.decode('utf-8'))
+            print("Nhận: ", data.decode('utf-8'))
     except:
         pass
     finally:
         ssl_socket.close()
-        print("Kết nói đã đóng.")
-
+        print("Kết nối đã đóng")
+        
+        
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-context.verify_mode = ssl.CERT_NONE  
-context.check_hostname = False  
 
-ssl_socket = context.wrap_socket(client_socket, server_hostname="localhost")
+context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+context.verify_mode = ssl.CERT_NONE
+context.check_hostname = False
+
+
+ssl_socket = context.wrap_socket(client_socket, server_hostname='localhost')
 
 ssl_socket.connect(server_address)
 
+
 receive_thread = threading.Thread(target=receive_data, args=(ssl_socket,))
 receive_thread.start()
-try:
+
+
+try: 
     while True:
-        message = input("Nhập tin nhăn: ")
+        message = input("Nhập tin nhắn: ")
         ssl_socket.send(message.encode('utf-8'))
 except KeyboardInterrupt:
     pass
 finally:
     ssl_socket.close()
+
